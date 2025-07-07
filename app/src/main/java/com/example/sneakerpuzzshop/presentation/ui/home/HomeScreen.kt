62cbfb72d6066da6312c76dd67b2a312ae67cb2b
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2025-07-07 12:55:57 +0700

    HomeScreen (navigate to pages)

--- app/src/main/java/com/example/sneakerpuzzshop/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/sneakerpuzzshop/presentation/ui/home/HomeScreen.kt
@@ -1,39 +1,79 @@
 package com.example.sneakerpuzzshop.presentation.ui.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person2
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavController
+import com.example.sneakerpuzzshop.presentation.ui.pages.CartPage
+import com.example.sneakerpuzzshop.presentation.ui.pages.FavoritePage
+import com.example.sneakerpuzzshop.presentation.ui.pages.HomePage
+import com.example.sneakerpuzzshop.presentation.ui.pages.ProfilePage
 import com.example.sneakerpuzzshop.presentation.viewmodel.AuthViewModel
 
 @Composable
 fun HomeScreen(viewModel: AuthViewModel?, navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Something")
-        TextButton(
-            onClick = {
-                viewModel?.signOut()
-                navController.navigate("login") {
-                    popUpTo("home") { inclusive = true }
+
+    val navItemList = listOf<NavItem>(
+        NavItem("Home", Icons.Default.Home),
+        NavItem("Favorite", Icons.Default.Favorite),
+        NavItem("Cart", Icons.Default.ShoppingCart),
+        NavItem("Profile", Icons.Default.Person2)
+    )
+
+    var selectedIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                navItemList.forEachIndexed { index, navItem ->
+                    NavigationBarItem(
+                        selected = index == selectedIndex,
+                        onClick = {
+                            selectedIndex = index
+                        },
+                        icon = {
+                            Icon(imageVector = navItem.icon, contentDescription = navItem.label)
+                        },
+                        label = {
+                            Text(text = navItem.label)
+                        }
+                    )
                 }
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = "Sign out", fontSize = 18.sp)
+            }
         }
+    ) {
+        ContentScreen(modifier = Modifier.padding(it), selectedIndex)
     }
 }
+
+@Composable
+fun ContentScreen(modifier: Modifier, index: Int) {
+    when(index) {
+        0 -> HomePage(modifier)
+        1 -> FavoritePage(modifier)
+        2 -> CartPage(modifier)
+        3 -> ProfilePage(modifier)
+    }
+}
+
+data class NavItem(
+    val label: String,
+    val icon: ImageVector
+)
