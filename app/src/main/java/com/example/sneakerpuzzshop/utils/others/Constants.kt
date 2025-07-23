6f43c7b43015e2fbac4e98434179dd7d904606be
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2025-07-23 17:40:39 +0700

    order feature: create order status list

--- app/src/main/java/com/example/sneakerpuzzshop/utils/others/Constants.kt
+++ app/src/main/java/com/example/sneakerpuzzshop/utils/others/Constants.kt
@@ -2,3 +2,10 @@ package com.example.sneakerpuzzshop.utils.others
 
 const val CLOUDINARY_PRODUCT = "dbqivrs0v"
 const val UPLOAD_PRESET = "sneaker_puzz"
+
+val ORDER_STATUS_LIST = listOf<String>(
+    "Pending",
+    "Delivered",
+    "Processing",
+    "Cancelled"
+)
