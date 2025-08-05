4feedf366ecb3f73b918a25c8807b36014731118
Dung Nguyen <2101140019@ms.hanu.edu.vn>
2025-08-05 22:08:26 +0700

    add back press handler (2 times -> kill app)

--- app/src/main/java/com/example/sneakerpuzzshop/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/sneakerpuzzshop/presentation/ui/home/HomeScreen.kt
@@ -1,5 +1,8 @@
 package com.example.sneakerpuzzshop.presentation.ui.home
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -14,18 +17,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
+import com.example.sneakerpuzzshop.presentation.components.showToast
 import com.example.sneakerpuzzshop.presentation.ui.pages.CartPage
 import com.example.sneakerpuzzshop.presentation.ui.pages.FavoritePage
 import com.example.sneakerpuzzshop.presentation.ui.pages.HomePage
 import com.example.sneakerpuzzshop.presentation.ui.pages.ProfilePage
 
+@SuppressLint("ContextCastToActivity")
 @Composable
 fun HomeScreen(navController: NavHostController) {
+
+    val context = LocalContext.current
+    val activity = (LocalContext.current as? Activity)
 
     val navItemList = listOf<NavItem>(
         NavItem("Home", Icons.Default.Home),
@@ -36,6 +46,17 @@ fun HomeScreen(navController: NavHostController) {
 
     var selectedIndex by rememberSaveable {
         mutableStateOf(0)
+    }
+
+    var backPressedTime by remember { mutableStateOf(0L) }
+
+    BackHandler(enabled = true) {
+        if (System.currentTimeMillis() - backPressedTime < 2000) {
+            activity?.finish()
+        } else {
+            showToast(context = context, message = "Nhấn lần nữa để thoát")
+            backPressedTime = System.currentTimeMillis()
+        }
     }
 
     Scaffold(
